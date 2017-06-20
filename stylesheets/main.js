b02a715d68d14c997dbd51f19c22e7c5cb441425
Katy Pola <katyp03@gmail.com>
2017-06-20 17:29:39 -0400

    game is functional; fixed letter guesses filling to corresponding cells, placeholder cells appearing with hint, guessCounter showing remaining guesses, and failBar showing bar that decreases with each wrong guess

--- stylesheets/main.js
+++ stylesheets/main.js
@@ -47,14 +47,9 @@
 // var myRound = new Round;
 // myRound.addWord(new Word('porpoise', 'Animal'),new Word('tiger', 'Animal'));
 
+// var letterIndex = word.secretWord.indexOf(letterGuess);
+// 			// console.log(letterIndex);
 
-
-let guessbutton;
-document.addEventListener("DOMContentLoaded", function(){
-	var words = [{secretWord: "Dog", hint: "Animal"}, {secretWord: "Pineapple", hint: "Fruit"}, {secretWord: "Barcelona", hint: "Place"}, {secretWord: "Marigold", hint: "Color"}]
-	// to get a random word on page load:
-	var word = words[Math.floor(Math.random()*(words.length-1))];
-		console.log(word);
 
 		// ALTERNATE METHOD:
 	// randomly choose one of the words
@@ -64,6 +59,20 @@ document.addEventListener("DOMContentLoaded", function(){
 	// var secretWord = "Word";
 	// var hint = "Animal";
 
+// qmark.filter( function(_,i){
+				// 	return i >= secretWordLength;
+				// }).forEach( function(el) {
+				// 	el.innerText = '';
+				// });
+
+
+let guessbutton;
+document.addEventListener("DOMContentLoaded", function(){
+	// define key value pairs for different secret words and hints:
+	var words = [{secretWord: "Porpoise", hint: "Animal"}, {secretWord: "Pineapple", hint: "Fruit"}, {secretWord: "Barcelona", hint: "Place"}, {secretWord: "Marigold", hint: "Color"}]
+	// to get a random word on page load:
+	var word = words[Math.floor(Math.random()*(words.length-1))];
+		console.log(word);
 	guessbutton = document.querySelector("#buttonGuess");
 	space1 = document.querySelector("#space1");
 	space2 = document.querySelector("#space2");
@@ -77,51 +86,94 @@ document.addEventListener("DOMContentLoaded", function(){
 	space10 = document.querySelector("#space10");
 	space11 = document.querySelector("#space11");
 	space12 = document.querySelector("#space12");
+	var guessCounter = 6;
+	var failBar = document.getElementById('failBar');
+	secretWordLength = word.secretWord.length;
 	guessForm = document.querySelector("#formGuess");
 	hintText = document.querySelector("#texthint");
+	guessesLeft = document.getElementById('guessesLeft');
+	console.log(secretWordLength);
+	// display hint on click:
 	hintText.addEventListener("click", function(){
 		console.log("Clicking on hint is working");
-		// ALTERNATE METHOD:
-		// hintText.innerText = words[0]["hint"];
-
 		hintText.innerText = word["hint"];
+		// show question mark placeholders or white spaces to indicate length of word:
+		document.querySelectorAll('.qmark').forEach( (el,i) => {
+			if( i >= secretWordLength ) {
+				el.innerText='';
+				el.classList.remove('splash2');
+				el.classList.add('white');
+		}
+	});
 	});
 	guessbutton.addEventListener("click", function(){
+		guessesLeft.innerText = guessCounter;
+		// define letterGuess as the letter the user guesses, and make sure it's one character in length:
 		letterGuess = guessForm.querySelector("input[type=text]").value;
 		if( letterGuess.length === 0 ) {
 			alert ("A blank space?  Really?  Let's try this again.");
 		} else if( letterGuess.length > 1 ) {
 			alert ("COME ON NOW.  One letter at a time.");
 		} 
-		else if( letterGuess.length === 1 && word.secretWord.includes(letterGuess) ){
+		// if letterGuess is correct length and is in secretWord:
+		else if( letterGuess.length === 1 && word.secretWord.toLowerCase().includes(letterGuess) ){
 			console.log(letterGuess);
-			var letterIndex = word.secretWord.indexOf(letterGuess);
-			console.log(letterIndex);
-
-			// get index of letterGuess in secretWord;
+			// get all index positions of letterGuess in secretWord;
 			function charPos(secretWord, letterGuess) {
   				return secretWord.toLowerCase()
          		.split("").map(function (letter, i) { if (letter == letterGuess) return i;})
          		.filter(function (v) { return v >= 0; });
 			}
+
 			var indices = charPos(word.secretWord, letterGuess);
 			console.log(indices);
-
-			// have secretWord display:none in #row4, so that letter at index[0] is in #space1, letter index[1] is in #space2, and so on; if there's a hidden letter in a space (so if space contains display:none), have some kind of indicator (maybe a space without display:none can have .white class)
-
-			// SUCCESS CONDITION
-			// check if letter in word
-			// if true display in appropriate box
-			// save letter somewhere? (in an array?)- necessary?
+				if ( indices.includes(0) ){
+					space1.innerText = letterGuess;
+				}
+				if ( indices.includes(1) ){
+					space2.innerText = letterGuess;
+				}
+				if ( indices.includes(2) ){
+					space3.innerText = letterGuess;
+				}
+				if ( indices.includes(3) ){
+					space4.innerText = letterGuess;
+				}
+				if ( indices.includes(4) ){
+					space5.innerText = letterGuess;
+				}
+				if ( indices.includes(5) ){
+					space6.innerText = letterGuess;
+				}
+				if ( indices.includes(6) ){
+					space7.innerText = letterGuess;
+				}
+				if ( indices.includes(7) ){
+					space8.innerText = letterGuess;
+				}
+				if ( indices.includes(8) ){
+					space9.innerText = letterGuess;
+				}
+				if ( indices.includes(9) ){
+					space10.innerText = letterGuess;
+				}
+				if ( indices.includes(10) ){
+					space11.innerText = letterGuess;
+				}
+				if ( indices.includes(11) ){
+					space12.innerText = letterGuess;
+				}
 		}
 		else {
-			console.log("letterGuess doesn't match");
 			// correct letterGuess length (1 character) but the letter doesn't match a letter in the array
-			// use contain and/or test, indexOf
-		// 	// decrease guessesLeft by 1, fill guess bar another 1/6th and have consolation phrase (eg, "Sorry, try again") display on fadeIn and fadeOut
-		// }
+			console.log(guessCounter);
+			guessCounter -= 1;
+			guessesLeft.innerText = guessCounter;
+			failBar.className = ("col-xs-" + (guessCounter*2));
+			if (guessCounter == 0){
+				alert("YOU'RE SO STUPID.");
+			}
 		}
+		document.querySelector("input[name=letter]").value = '';
 	});
 });
-
-// counter for 
